03bf137db886a74092eafaf242aecc35a793ee97
devnull <devnull@example.com>
2026-03-31 19:27:56 -0600

    Creacion: se inioc el proyecto con los archivos nuevos base y la estrucura de las carpetas

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+//configuracion de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'nombre_de_la_base_de_datos');
+define('DB_USER', 'usuario_de_la_base_de_datos');
+define('DB_PASS', 'contraseña_de_la_base_de_datos');
